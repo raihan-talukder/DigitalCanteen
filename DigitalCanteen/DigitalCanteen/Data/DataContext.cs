537bcee7c920ae8f071fa405ae6b1ef0dee4bc6e
judithsharlz <33415185+judithsharlz@users.noreply.github.com>
2017-11-12 13:27:50 +0600

    Balance class created

---
+++ DigitalCanteen/DigitalCanteen/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using DigitalCanteen.Models;
+
+namespace DigitalCanteen.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext()
+            : base("DataContext")
+        {
+            Database.SetInitializer<DataContext>(new CreateDatabaseIfNotExists<DataContext>());
+        }
+        public  DbSet<UserCredential> UserCredentials { get; set; }
+        public virtual DbSet<UserDetail> UserDetails { get; set; }
+   
+      
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
