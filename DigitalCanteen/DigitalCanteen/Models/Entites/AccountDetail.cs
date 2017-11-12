2a8e10325284cb40eb5d629afc41b135ce4f4048
maruf <maruf>
2017-11-12 14:42:28 +0600

    model class added

---
+++ DigitalCanteen/DigitalCanteen/Models/Entites/AccountDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DigitalCanteen.Models.Entites
+{
+    public class AccountDetail
+    {
+        [Key, ForeignKey("UserDetails")]
+        public int UserId { get; set; }
+
+        public int Balance { get; set; }
+        public int Refil { get; set; }
+
+        public virtual UserDetail UserDetail { get; set; }
+        public virtual ICollection<Item> Items { get; set; } 
+    }
+}
---
+++ DigitalCanteen/DigitalCanteen/Models/Entites/UserCredential.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DigitalCanteen.Models.Entites
+{
+    public class UserCredential
+    {
+        [Key, ForeignKey("UserDetails")]
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public bool Active { get; set; }
+        public string UserType { get; set; }
+
+        public virtual UserDetail UserDetail { get; set; }
+    }
+}
